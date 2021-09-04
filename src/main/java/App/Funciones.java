8c0e97510564d1dd98bfdfa06b33eae74999a9c0
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-04 00:15:08 -0500

    Metodos del 1 al 5 desarrollados

--- src/main/java/App/Funciones.java
+++ src/main/java/App/Funciones.java
@@ -1,5 +1,54 @@
 package App;
 
-public class Funciones {
+import static java.lang.System.*;
 
+public class Funciones {
+    public void Mayor(){
+        int x = 20;
+        int y = 14;
+
+        if (x == y){
+            out.println("los numeros ingresados son iguales");
+        }else if (x > y){
+            out.println("El numero mayor es: " + x);
+            out.println("El numero menor es: " + y);
+        }else{
+            out.println("El numero mayor es: " + y);
+            out.println("El numero menor es: " + x);
+        }
+    }
+    public void Mayor(double x, double y){
+        if (x == y){
+            out.println("los numeros ingresados son iguales");
+        }else if (x < y){
+            out.println("El numero mayor es: " + x);
+            out.println("El numero menor es: " + y);
+        }else{
+            out.println("El numero mayor es: " + y);
+            out.println("El numero menor es: " + x);
+        }
+    }
+
+    public void CalcularAreaCirculo(double radio){
+        double res = Math.PI * Math.pow(radio,2);
+        out.println("El area del circulo es: " + res);
+    }
+
+    public void CalcularPrecioTotalConIVA(double precio){
+        double res = precio + (precio * 0.21);
+        out.println("El precio final es: " + res);
+    }
+
+    public void MostrarParesImpares(){
+        boolean bandera = true;
+        double actual = 1;
+        while (actual <= 100 ){
+            if (actual % 2 == 0){
+                out.println("El numero " + actual + " es par");
+            }else {
+                out.println("El numero " + actual + " es impar");
+            }
+            actual++;
+        }
+    }
 }
--- src/main/java/App/TallerEvaluacionJava.java
+++ src/main/java/App/TallerEvaluacionJava.java
@@ -1,7 +1,95 @@
 package App;
 
+import java.util.Scanner;
+import static java.lang.System.*;
+
 public class TallerEvaluacionJava {
     public static void main(String[] args) {
-        
+        Funciones obj = new Funciones();
+        int seleccion;
+        do {
+            out.println("Seleccione un item del menu");
+            out.println("1- Punto 1\n2- Punto 2\n3- Punto 3\n4- Punto 4\n5- Punto 5\n" +
+                    "6- Punto 6\n7- Punto 7\n8- Punto 8\n9- Punto 9\n10- Punto 10\n" +
+                    "11- Punto 11\n12- Punto 12\n13- Punto 13\n14- Punto 14\n15- Punto 15\n" +
+                    "16- Punto 16\n17- Punto 17\n18- Punto 18\n19- Salir");
+            Scanner scanner = new Scanner(System.in);
+            seleccion = scanner.nextInt();
+
+            switch (seleccion) {
+                case 1:
+                    obj.Mayor();
+                    break;
+                case 2:
+                    try {
+                        String numero1;
+                        String numero2;
+                        out.println("Por favor ingrese el primer numero");
+                        numero1 = scanner.next();
+                        out.println("Por favor ingrese el segundo numero");
+                        numero2 = scanner.next();
+
+                        obj.Mayor(Double.parseDouble(numero1.replace(",",".")), Double.parseDouble(numero2.replace(",",".")));
+                    }catch (Exception ex){
+                        out.println("Por favor ingresar solo numeros");
+                    }
+                    break;
+                case 3:
+                    try {
+                        String radio;
+                        out.println("Por favor ingrese el valor del radio");
+                        radio = scanner.next();
+
+                        obj.CalcularAreaCirculo(Double.parseDouble(radio.replace(",",".")));
+                    }catch (Exception ex){
+                        out.println("Por favor ingresar solo numeros");
+                    }
+                    break;
+                case 4:
+                    try {
+                        String precio;
+                        out.println("Por favor ingrese el precio");
+                        precio = scanner.next();
+
+                        obj.CalcularPrecioTotalConIVA(Double.parseDouble(precio.replace(",",".")));
+                    }catch (Exception ex){
+                        out.println("Por favor ingresar solo numeros");
+                    }
+                    break;
+                case 5:
+                    obj.MostrarParesImpares();
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    break;
+                default:
+                    out.println("No ha ingresado ninguna opcion valida, recuerde que van desde el numero 1 al 19");
+                    break;
+
+            }
+        } while (seleccion != 19);
     }
 }
