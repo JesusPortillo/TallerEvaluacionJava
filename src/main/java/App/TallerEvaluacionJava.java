278505fa1b034efd36a504074aad741e83396ce2
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-07 20:21:14 -0500

    Punto 16 desarrollado

--- src/main/java/App/TallerEvaluacionJava.java
+++ src/main/java/App/TallerEvaluacionJava.java
@@ -32,8 +32,8 @@ public class TallerEvaluacionJava {
                             numero1 = scanner.next();
                             out.println("Por favor ingrese el segundo numero");
                             numero2 = scanner.next();
-                            obj.Mayor(Double.parseDouble(numero1.replace(",",".")), Double.parseDouble(numero2.replace(",",".")));
-                        }catch (Exception ex){
+                            obj.Mayor(Double.parseDouble(numero1.replace(",", ".")), Double.parseDouble(numero2.replace(",", ".")));
+                        } catch (Exception ex) {
                             out.println("Por favor ingresar solo numeros");
                         }
                         break;
@@ -42,8 +42,8 @@ public class TallerEvaluacionJava {
                             String radio;
                             out.println("Por favor ingrese el valor del radio");
                             radio = scanner.next();
-                            obj.CalcularAreaCirculo(Double.parseDouble(radio.replace(",",".")));
-                        }catch (Exception ex){
+                            obj.CalcularAreaCirculo(Double.parseDouble(radio.replace(",", ".")));
+                        } catch (Exception ex) {
                             out.println("Por favor ingresar solo numeros");
                         }
                         break;
@@ -52,8 +52,8 @@ public class TallerEvaluacionJava {
                             String precio;
                             out.println("Por favor ingrese el precio");
                             precio = scanner.next();
-                            obj.CalcularPrecioTotalConIVA(Double.parseDouble(precio.replace(",",".")));
-                        }catch (Exception ex){
+                            obj.CalcularPrecioTotalConIVA(Double.parseDouble(precio.replace(",", ".")));
+                        } catch (Exception ex) {
                             out.println("Por favor ingresar solo numeros");
                         }
                         break;
@@ -70,7 +70,7 @@ public class TallerEvaluacionJava {
                         String dia;
                         out.println("Ingrese un dia de la semana");
                         dia = scanner.next();
-                        if (dia.chars().allMatch(Character::isDigit)){
+                        if (dia.chars().allMatch(Character::isDigit)) {
                             out.println("Ingresar algo en formato numerico no es valido aqui");
                         }
                         obj.VerificarDiaLaboral(dia);
@@ -80,9 +80,9 @@ public class TallerEvaluacionJava {
                         out.println("Ingrese una frase");
 
                         frase = scanner.next();
-                        if (frase.chars().allMatch(Character::isDigit)){
+                        if (frase.chars().allMatch(Character::isDigit)) {
                             out.println("Ingresar algo en formato numerico no es valido aqui");
-                        }else {
+                        } else {
                             obj.ConcatenerFraseConOtra(frase);
                         }
                         break;
@@ -90,9 +90,9 @@ public class TallerEvaluacionJava {
                         String fraseConEspacios;
                         out.println("Ingrese una frase");
                         fraseConEspacios = scanner.next();
-                        if (fraseConEspacios.chars().allMatch(Character::isDigit)){
+                        if (fraseConEspacios.chars().allMatch(Character::isDigit)) {
                             out.println("Ingresar algo en formato numerico no es valido aqui");
-                        }else {
+                        } else {
                             obj.EliminarEspacios(fraseConEspacios);
                         }
                         break;
@@ -100,9 +100,9 @@ public class TallerEvaluacionJava {
                         String frasePorContar;
                         out.println("Ingrese una frase");
                         frasePorContar = scanner.next();
-                        if (frasePorContar.chars().allMatch(Character::isDigit)){
+                        if (frasePorContar.chars().allMatch(Character::isDigit)) {
                             out.println("Ingresar algo en formato numerico no es valido aqui");
-                        }else {
+                        } else {
                             obj.ContarVocalesConLongitud(frasePorContar);
                         }
                         break;
@@ -113,9 +113,9 @@ public class TallerEvaluacionJava {
                         frase1 = scanner.next();
                         out.println("Ingrese frase 2 a comparar");
                         frase2 = scanner.next();
-                        if (frase1.chars().allMatch(Character::isDigit) || frase2.chars().allMatch(Character::isDigit)){
+                        if (frase1.chars().allMatch(Character::isDigit) || frase2.chars().allMatch(Character::isDigit)) {
                             out.println("Ingresar algo en formato numerico no es valido aqui");
-                        }else {
+                        } else {
                             obj.IgualoDiferente(frase1, frase2);
                         }
                         break;
@@ -128,7 +128,7 @@ public class TallerEvaluacionJava {
                             out.println("Ingrese el valor inicial");
                             inicio = scanner.nextDouble();
                             obj.ContarHastaMil(inicio);
-                        }catch (Exception ex){
+                        } catch (Exception ex) {
                             out.println("Por favor ingresar solo numeros");
                         }
                         break;
@@ -136,6 +136,76 @@ public class TallerEvaluacionJava {
                         obj.menu();
                         break;
                     case 16:
+                        try {
+                            String entrada;
+                            int tipoPeso1 = 0;
+                            int tipoPeso2 = 0;
+                            int tipoPeso3 = 0;
+                            boolean esMayorDeEdad = true;
+                            System.out.print("Ingrese el nombre de la persona: ");
+                            String nombre = scanner.next();
+                            System.out.print("Ingrese la edad de la persona: ");
+                            int edad = scanner.nextInt();
+                            while (edad < 1) {
+                                System.out.println("La edad no es valida");
+                                System.out.print("Ingrese la edad de la persona: ");
+                                edad = scanner.nextInt();
+                            }
+                            System.out.print("Ingrese el sexo de la persona siendo \"H\" hombre y \"M\" mujer : ");
+                            char sexo = scanner.next().charAt(0);
+                            System.out.print("Ingrese el peso de la persona en KG: ");
+                            int peso = scanner.nextInt();
+                            while (peso < 3) {
+                                System.out.println("El peso no es valido");
+                                System.out.print("Ingrese el peso de la persona en KG: ");
+                                peso = scanner.nextInt();
+                            }
+                            System.out.print("Ingrese la altura de la persona en metros: ");
+                            entrada = scanner.next();
+                            entrada = entrada.replace(",", ".");
+                            float altura = Float.parseFloat(entrada);
+                            while (altura < 0.40) {
+                                System.out.println("la altura no es valida");
+                                System.out.print("Ingrese la altura de la persona en metros: ");
+                                altura = scanner.nextFloat();
+                            }
+                            Persona persona1 = new Persona(nombre, edad, peso, altura, sexo);
+                            Persona persona2 = new Persona(nombre, edad, sexo);
+                            Persona persona3 = new Persona();
+                            persona3.setNombre(nombre);
+                            persona3.setEdad(edad);
+                            persona3.setPeso(peso);
+                            persona3.setAltura(altura);
+                            persona3.setSexo(sexo);
+
+                            tipoPeso1 = persona1.calcularIMC();
+                            System.out.println("Persona 1 - " + comprobarPeso(tipoPeso1));
+                            tipoPeso2 = persona2.calcularIMC();
+                            System.out.println("Persona 2 - " + comprobarPeso(tipoPeso2));
+                            tipoPeso3 = persona3.calcularIMC();
+                            System.out.println("Persona 3 - " + comprobarPeso(tipoPeso3));
+
+                            esMayorDeEdad = persona1.esMayorDeEdad();
+                            if (esMayorDeEdad) {
+                                System.out.println("La persona 1 es mayor de edad");
+                            }
+                            esMayorDeEdad = persona2.esMayorDeEdad();
+                            if (esMayorDeEdad) {
+                                System.out.println("la persona 2 es mayor de edad");
+                            }
+                            esMayorDeEdad = persona3.esMayorDeEdad();
+                            if (esMayorDeEdad) {
+                                System.out.println("La persona 3 es mayot de edad");
+                            }
+
+                            System.out.println(persona1.toString());
+                            System.out.println(persona2.toString());
+                            System.out.println(persona3.toString());
+
+
+                        } catch (Exception e) {
+                            System.out.println("Ocurrio un error " + e);
+                        }
                         break;
                     case 17:
                         break;
@@ -146,10 +216,19 @@ public class TallerEvaluacionJava {
                         break;
 
                 }
-            }catch (Exception ex){
+            } catch (Exception ex) {
                 out.println("Ingrese una opcion valida");
             }
         } while (seleccion != 19);
+    }
+    public static String comprobarPeso(int n) {
+        String respuesta = "La persona esta en sobrepeso";
+        if (n == -1) {
+            respuesta = "La persona esta por debajo de su peso ideal";
+        } else if (n == 0) {
+            respuesta = "La persona esta en su peso ideal";
+        }
+        return respuesta;
     }
 
 }
